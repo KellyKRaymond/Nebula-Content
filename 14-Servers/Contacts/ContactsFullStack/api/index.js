f2415fbfe330822debe55dc094fe58abb46ca119
KellyKRaymond <kellyraymondis@gmail.com>
2022-02-15 14:23:10 -0500

    class example

--- 14-Servers/Contacts/ContactsFullStack/api/index.js
+++ 14-Servers/Contacts/ContactsFullStack/api/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const db = require('./queries.js');
 const bodyParser = require('body-parser');
+const cors = require('cors')
+
+
 
 const app = express();
 const port = 3030;
 
+
 app.use(bodyParser.json());
+app.use(cors()); 
+
 app.get('/', (request, response) => { response.json({ info: 'Node.js, Express and Postgres API'})})
 app.get('/contacts', db.getContacts);
 app.get('/contact', db.getContact);
